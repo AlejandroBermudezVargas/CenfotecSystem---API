ee4fc4433cc4a976b7fd9d78f4c3075c9cf76a39
unknown <abermudezv@ucenfotec.ac.cr>
2015-11-12 00:19:34 -0600

    Add method in bitacora to involucrate two users un some actions

--- WebAPI-Sistema-Cenfotec/Controllers/Logic/Bitacora.cs
+++ WebAPI-Sistema-Cenfotec/Controllers/Logic/Bitacora.cs
@@ -26,5 +26,15 @@ namespace WebAPI_Sistema_Cenfotec.Controllers.Logic
             db.bitacoras.Add(bitacora);
             db.SaveChanges();
         }
+        public void addBitacora(string paccion, int puserId, int puserIdTwo) //Two users
+        {
+            bitacora bitacora = new bitacora();
+            bitacora.accion = paccion;
+            bitacora.fecha = DateTime.Now;
+            bitacora.id_usuario = puserId;
+            bitacora.id_usuario_afectado = puserIdTwo;
+            db.bitacoras.Add(bitacora);
+            db.SaveChanges();
+        }
     }
 }
